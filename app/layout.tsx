0509e779654fc8cfe9d8a2a3a04ee02e0d6c4bef
k-jipgeria <sgim43547@gmail.com>
2025-01-16 16:26:43 +0900

    feat: 가변 다이나믹 pretendard font 적용 => 렌더링에 필요한 최소한의 폰트만 다운로드

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,10 @@
 import './globals.css';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Coworkers',
+  description: 'Coworkers is a platform for team collaboration',
+};
 
 export default function RootLayout({
   children,
@@ -7,6 +13,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Coworkers</title>
+        <link
+          rel="preconnect"
+          href="https://cdn.jsdelivr.net"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          as="style"
+          crossOrigin="anonymous"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.8/dist/web/variable/pretendardvariable-dynamic-subset.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.8/dist/web/variable/pretendardvariable-dynamic-subset.css"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
