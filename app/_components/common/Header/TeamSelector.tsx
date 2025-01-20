9efd246c5417fd1c12f96b060795afa6fca90999
k-jipgeria <sgim43547@gmail.com>
2025-01-21 00:07:39 +0900

    feat: 헤더 팀 선택  컴포넌트 구현
    - 아직 로그인 기능이 미완이므로 테스트 목적으로 mock data 추가한 상태
    - 로그인 기능 완료 시, 유저가 속한 팀 목록을 보여주는 기능으로 전환 예정

---
+++ app/_components/common/Header/TeamSelector.tsx
@@ -0,0 +1,129 @@
+'use client';
+
+import { useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import Dropdown from '@components/common/Dropdown';
+import toggle from '@icons/toggle.svg'
+import defaultTeamIcon from '@icons/default-team-icon.svg'
+
+
+const mockUser = {
+  teamId: "team-1",
+  image: "https://picsum.photos/200",
+  nickname: "Test User",
+  email: "test@example.com",
+  id: 1,
+  memberships: [
+    {
+      group: {
+        teamId: "team-1",
+        image: "",
+        name: "인사팀",
+        id: 1,
+        createdAt: "2024-01-01",
+        updatedAt: "2024-01-01"
+      },
+      role: "ADMIN",
+      userImage: "https://picsum.photos/200",
+      userEmail: "test@example.com",
+      userName: "Test User",
+      groupId: 1,
+      userId: 1
+    },
+    {
+      group: {
+        teamId: "team-2",
+        image: "https://picsum.photos/200/300",
+        name: "재무팀",
+        id: 2,
+        createdAt: "2024-01-01",
+        updatedAt: "2024-01-01"
+      },
+      role: "ADMIN",
+      userImage: "https://picsum.photos/200",
+      userEmail: "test@example.com",
+      userName: "Test User",
+      groupId: 2,
+      userId: 1
+    }
+  ]
+};
+
+const TeamSelector: React.FC = () => {
+  const [selectedTeam, setSelectedTeam] = useState(mockUser.memberships[0].group.name);
+  
+  // if (!user?.memberships.length) {
+  //   return (
+  //     <Link href="/addteam">
+  //       팀 생성
+  //     </Link>
+  //   )
+  // }
+
+  return (
+    <div aria-label="팀 선택">
+      <Dropdown>
+        {({ isOpen, toggleDropdown }) => (
+          <>
+            <Dropdown.Button 
+              onClick={toggleDropdown}
+              aria-expanded={isOpen}
+              aria-haspopup="true"
+            >
+              <div className="flex items-center justify-center gap-2">
+                <span>{selectedTeam}</span>
+                <Image 
+                  src={toggle}
+                  alt="토글 아이콘"
+                  width={24}
+                  height={24}
+                  sizes="2.4rem"
+                  className={`transition-transform duration-200 ${isOpen ? 'rotate-180' : ''}`}
+                />
+              </div>
+            </Dropdown.Button>
+            <Dropdown.Menu
+              isOpen={isOpen}
+              boxClass="w-[22rem] top-[4rem] p-4 flex flex-col gap-2 items-center"
+            >
+              {mockUser.memberships.map((membership) => (
+                <Link 
+                  href={`/team/${membership.group.id}`} 
+                  key={membership.group.id}
+                  className="w-full"
+                >
+                  <Dropdown.Item 
+                    onClick={() => setSelectedTeam(membership.group.name)} 
+                    toggleDropdown={toggleDropdown}
+                    className="flex items-center justify-between gap-4 w-[18.6rem] h-[4.6rem] rounded-xl">
+                    <div className="flex items-center gap-4">
+                      <Image 
+                        src={membership.group.image || defaultTeamIcon}
+                        alt={membership.group.image ? `${membership.group.name} 팀 이미지` : "기본 팀 아이콘"}
+                        width={32}
+                        height={32}
+                        sizes="3.2rem"
+                        className="rounded-md size-[3.2rem] object-cover"
+                      />
+                      <span className="font-medium">{membership.group.name}</span>
+                    </div>
+                    <span className="text-icon-primary">⁝</span>
+                  </Dropdown.Item>
+                </Link>
+              ))}
+              
+              <Link href="/addteam">
+                <Dropdown.Item toggleDropdown={toggleDropdown} className="w-[18.6rem] h-[4.6rem] rounded-xl border border-brand-primary justify-center mt-4">
+                  + 팀 추가하기
+                </Dropdown.Item>
+              </Link>
+            </Dropdown.Menu>
+          </>
+        )}
+      </Dropdown>
+    </div>
+  )
+};
+
+export default TeamSelector;
