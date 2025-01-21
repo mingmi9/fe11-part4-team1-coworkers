671df94de2c668a9e6026cea9cac3c16c5e16158
김예도 <kyd629@naver.com>
2025-01-21 18:08:08 +0900

    fix: 새로운 목록 추가 작성

--- app/_components/TeamPage/TodoListSection.tsx
+++ app/_components/TeamPage/TodoListSection.tsx
@@ -12,8 +12,13 @@ interface TodoListSectionProps {
 export default function TodoListSection({tasks} : TodoListSectionProps) {
   return (
     <div>
-        <div className="text-text-primary text-[1.6rem] mb-[1.6rem] w-[120rem]">
-            할 일 목록 <span className="text-text-default">({tasks.length}개)</span>
+        <div className="flex justify-between mb-[1.6rem] w-[120rem]">
+            <div className="text-text-primary text-[1.6rem]">
+                할 일 목록 <span className="text-text-default">({tasks.length}개)</span>
+            </div>
+            <button className="text-brand-primary hover:brightness-150">
+                + 새로운 목록 추가하기
+            </button>
         </div>
         <div className="flex flex-col gap-[1.6rem]">
         {tasks.map(( tasks, index ) => (
