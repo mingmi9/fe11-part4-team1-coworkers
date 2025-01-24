1e7f999c84fd2360c26f12c416f2df49bb08432d
김예도 <kyd629@naver.com>
2025-01-24 11:58:40 +0900

    fix: 진행상황 퍼센테이지 예외 처리 추가

--- app/_components/TeamPage/ReportSection.tsx
+++ app/_components/TeamPage/ReportSection.tsx
@@ -3,37 +3,44 @@ import done from '@images/img_done.png';
 import ReportCard from './ReportCard';
 
 interface ReportSectionProps {
-    alltasks: number;
-    completedtasks: number;
+  alltasks: number;
+  completedtasks: number;
 }
 
-export default function ReportSection ({alltasks, completedtasks}: ReportSectionProps) {
-    const graph = Math.round((completedtasks / alltasks) * 100);
-    return (
-        <div>
-            <div className="text-text-primary text-[1.6rem] mb-[2.4rem]">
-                리포트
+export default function ReportSection({
+  alltasks,
+  completedtasks,
+}: ReportSectionProps) {
+  const graph =
+    alltasks === 0 ? 0 : Math.round((completedtasks / alltasks) * 100);
+  return (
+    <div>
+      <div className="mb-[2.4rem] text-[1.6rem] text-text-primary">리포트</div>
+      <div className="flex h-[21.7rem] w-[120rem] items-center justify-between gap-[1rem] rounded-[1.2rem] bg-background-secondary p-[2.4rem]">
+        <div className="ml-[2rem] flex items-center gap-[3rem]">
+          <div className="relative h-[17rem] w-[17rem] rounded-full">
+            <div
+              className="absolute inset-0 h-full w-full rounded-full"
+              style={{
+                background: `conic-gradient(from 90deg, #a3e635, #22c55e ${graph}% 0%, #334155 ${graph}% 100%)`,
+              }}
+            />
+            <div className="absolute inset-11 flex items-center justify-center rounded-full bg-background-secondary" />
+          </div>
+          <div>
+            <div className="text-[1.4rem] text-text-primary">
+              오늘의 진행상황
             </div>
-            <div className="w-[120rem] h-[21.7rem] flex items-center justify-between bg-background-secondary rounded-[1.2rem] p-[2.4rem] gap-[1rem]">
-                <div className="flex items-center gap-[3rem] ml-[2rem]">
-                    <div className="relative w-[17rem] h-[17rem] rounded-full">
-                        <div className="absolute inset-0 w-full h-full rounded-full " style={{background: `conic-gradient(from 90deg, #a3e635, #22c55e ${graph}% 0%, #334155 ${graph}% 100%)`,}}/>
-                        <div className="absolute inset-11 flex items-center justify-center bg-background-secondary rounded-full"/>
-                    </div>
-                    <div>
-                        <div className="text-text-primary text-[1.4rem]">
-                            오늘의 진행상황
-                        </div>
-                        <div className="text-transparent bg-clip-text bg-gradient-to-r from-green-500 to-lime-400 text-[4rem] font-bold">
-                            {graph}%
-                        </div>
-                    </div>
-                </div>
-                <div className="flex flex-col gap-[1rem]">
-                    <ReportCard title="오늘의 할 일" value={alltasks} imgSrc={todo}/>
-                    <ReportCard title="한 일" value={completedtasks} imgSrc={done}/>
-                </div>
+            <div className="bg-gradient-to-r from-green-500 to-lime-400 bg-clip-text text-[4rem] font-bold text-transparent">
+              {graph}%
             </div>
+          </div>
         </div>
-    )
+        <div className="flex flex-col gap-[1rem]">
+          <ReportCard title="오늘의 할 일" value={alltasks} imgSrc={todo} />
+          <ReportCard title="한 일" value={completedtasks} imgSrc={done} />
+        </div>
+      </div>
+    </div>
+  );
 }
