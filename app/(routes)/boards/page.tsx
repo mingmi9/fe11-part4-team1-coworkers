399470b6877c45a52f641d17eb8c3693fd57e977
jeongmin <jmini9218@gmail.com>
2025-02-03 04:10:10 +0900

    feat: 게시판 페이지 ui구현

---
+++ app/(routes)/boards/page.tsx
@@ -0,0 +1,158 @@
+'use client';
+import { useState } from 'react';
+import Button from '@/_components/common/Button';
+import Search from '@/_components/boards/Search';
+import BestArticleList from '@/_components/boards/BestArticleList';
+import ArticleCard from '@/_components/boards/ArticleCard';
+import SortDropdown from '@/_components/boards/SortDropdown';
+
+const ArticleListPage = () => {
+  const handleButton = () => alert('글쓰기');
+  const [selectedOption, setSelectedOption] = useState<string>('최신순');
+  const handleSelect = (value: string) => {
+    setSelectedOption(value);
+    console.log('선택:', value);
+  };
+  const articles = [
+    {
+      createdAt: '2025-01-23T19:18:29.982Z',
+      likeCount: 15,
+      writer: {
+        nickname: '김코딩',
+        id: 1,
+      },
+      image: '/images/kakaotalk.png',
+      title:
+        '게시글 제목입니다게시글 제목입니다게시글 제목입니다게시글 제목입니다게시글 제목입니다게시글 제목입니다게시글 제목입니다게시글 제목입니다.',
+      id: 1,
+    },
+    {
+      createdAt: '2025-01-23T19:18:29.982Z',
+      likeCount: 15,
+      writer: {
+        nickname: '김코딩3',
+        id: 2,
+      },
+      title: '게시글 제목입니다.',
+      id: 2,
+    },
+    {
+      createdAt: '2025-01-23T19:18:29.982Z',
+      likeCount: 15,
+      writer: {
+        nickname: '김코딩',
+        id: 3,
+      },
+      image: '/article-image.jpg',
+      title: '게시글 제목입니다.',
+      id: 3,
+    },
+    {
+      createdAt: '2025-01-22T19:18:29.982Z',
+      likeCount: 3,
+      writer: {
+        nickname: '김코딩',
+        id: 5,
+      },
+      image: '/article-image2.jpg',
+      title: '게시글 제목입니다.',
+      id: 5,
+    },
+    {
+      createdAt: '2025-01-22T19:18:29.982Z',
+      likeCount: 3,
+      writer: {
+        nickname: '김코딩4',
+        id: 6,
+      },
+      image: '/article-image2.jpg',
+      title: '게시글 제목입니다.',
+      id: 6,
+    },
+    {
+      createdAt: '2025-01-22T19:18:29.982Z',
+      likeCount: 3,
+      writer: {
+        nickname: '김코딩',
+        id: 7,
+      },
+      image: '/article-image2.jpg',
+      title: '게시글 제목입니다.',
+      id: 7,
+    },
+  ];
+
+  return (
+    <div className="relative pt-[3.2rem] tablet:pt-[4rem]">
+      <h2 className="text-lg font-bold tablet:text-2xl">자유게시판</h2>
+      <div className="my-[2.4rem] tablet:my-[3.2rem] pc:my-[4rem]">
+        <Search />
+      </div>
+
+      <h3 className="mb-[2.4rem] font-medium tablet:mb-[4rem] tablet:text-xl tablet:font-semibold">
+        베스트 게시글
+      </h3>
+      <div>
+        <BestArticleList />
+      </div>
+
+      <div className="my-[3.2rem] h-[1px] w-full bg-text-primary opacity-[10%] tablet:my-[4rem]"></div>
+
+      <div className="mb-[2.4rem] flex items-center justify-between tablet:mb-[4rem]">
+        <h3 className="font-medium tablet:text-xl tablet:font-semibold">
+          게시글
+        </h3>
+        <div>
+          <SortDropdown
+            selectedOption={selectedOption}
+            onSelect={handleSelect}
+          />
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 gap-[1.6rem] tablet:gap-[2.4rem] tablet:gap-x-[2rem] pc:grid-cols-2">
+        {articles.map((article) => (
+          <ArticleCard
+            key={article.id}
+            article={article}
+            onClickMenu={() => console.log('clicked menu ' + article.id)}
+          />
+        ))}
+      </div>
+
+      <div className="fixed bottom-[6.2rem] right-[1.6rem] tablet:bottom-[4.5rem] tablet:right-[2.4rem] pc:right-[6.5rem]">
+        <Button
+          size="large"
+          icon="plus"
+          round="full"
+          onClick={handleButton}
+          className="w-[10.4rem]"
+        >
+          글쓰기
+        </Button>
+      </div>
+
+      {/* <BestArticleCard
+        key={articles[0].id}
+        article={articles[0]}
+        onClickMenu={() =>
+          console.log('Menu clicked for best ' + articles[0].id)
+        }
+      /> */}
+      {/* <BestArticleList /> */}
+      {/* <div className="space-y-4">
+        {articles.map((article) => (
+          <ArticleCard
+            key={article.id}
+            article={article}
+            onClickMenu={() =>
+              console.log('Menu clicked for article ' + article.id)
+            }
+          />
+        ))}
+      </div> */}
+    </div>
+  );
+};
+
+export default ArticleListPage;
