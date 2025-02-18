a6e9f6cd1af66a3bc8a62f00822a5f0d4164bd6a
jeongmin <jmini9218@gmail.com>
2025-02-18 23:45:51 +0900

    feat: 게시글 관련 카드 컴파운드패턴 컴포넌트 추가

--- app/_components/boards/Card.tsx
+++ app/_components/boards/Card.tsx
@@ -4,6 +4,8 @@ import Image from 'next/image';
 import { formatDate } from '@/_lib/utils/format-date';
 import classNames from 'classnames';
 
+const SubText = 'text-xs text-text-disabled font-normal tablet:text-sm';
+
 export const Medal = () => (
   <div className="mb-[1.4rem] flex items-center gap-[.4rem] text-sm font-semibold text-white">
     <Image src="/icons/ic-medal.svg" alt="medal" width={16} height={16} />
@@ -31,7 +33,7 @@ export const PreviewImage = ({ src }: { src: string }) => (
 
 export const Date = ({ date }: { date: string }) => {
   const formattedDate = formatDate(date, 'YYYY.M.D');
-  return <>{formattedDate}</>;
+  return <div className={`${SubText}`}>{formattedDate}</div>;
 };
 
 export const Profile = ({ nickname }: { nickname: string }) => (
@@ -45,7 +47,7 @@ export const Profile = ({ nickname }: { nickname: string }) => (
         className="object-cover"
       />
     </div>
-    <div className="text-sm font-medium">{nickname}</div>
+    <div className="text-xs font-medium tablet:text-sm">{nickname}</div>
   </div>
 );
 
@@ -56,7 +58,7 @@ export const LikeButton = ({
 }: {
   likeCount: number;
   liked: boolean;
-  onClick: () => void;
+  onClick?: () => void;
 }) => {
   const displayedLikeCount = likeCount > 9999 ? '9999+' : likeCount;
   return (
@@ -65,6 +67,7 @@ export const LikeButton = ({
       className={classNames(
         'flex items-center gap-[.4rem] text-xs font-normal tablet:text-sm',
         { 'text-red-500': liked, 'text-text-disabled': !liked },
+        { 'cursor-pointer': onClick, 'cursor-default': !onClick },
       )}
     >
       <Image
@@ -78,6 +81,54 @@ export const LikeButton = ({
   );
 };
 
+export const Divider = ({ className = '' }: { className?: string }) => {
+  return (
+    <div
+      className={`h-[1px] w-full bg-text-primary opacity-[10%] ${className}`}
+    />
+  );
+};
+
+export const DateDivider = () => {
+  return (
+    <span className="mx-[.8rem] h-[1.2rem] w-[.1rem] bg-background-tertiary tablet:mx-[1.6rem]"></span>
+  );
+};
+
+export const DetailImage = ({ src }: { src: string }) => (
+  <div className="relative ml-[1.6rem] size-[18rem] flex-shrink-0 overflow-hidden rounded-[.8rem] tablet:size-[50rem]">
+    <Image
+      src={src}
+      alt="게시글 이미지"
+      fill
+      sizes="18rem"
+      className="bg-blue-50 object-cover"
+    />
+  </div>
+);
+
+export const CommentText = ({ children }: { children: ReactNode }) => (
+  <div className="line-clamp-2 text-sm text-text-primary tablet:text-base">
+    {children}
+  </div>
+);
+
+export const CommentCount = ({
+  commentCount,
+  onClick,
+}: {
+  commentCount: number;
+  onClick?: () => void;
+}) => (
+  <button
+    onClick={onClick}
+    className={`flex items-center gap-[.4rem] ${SubText}`}
+  >
+    <Image src="/icons/ic-comment.svg" alt="댓글" width={16} height={16} />
+    {commentCount}
+  </button>
+);
+
 const Card = Object.assign(
   (props: { children: ReactNode }) => <>{props.children}</>,
   {
@@ -87,6 +138,11 @@ const Card = Object.assign(
     Date,
     Profile,
     LikeButton,
+    Divider,
+    DateDivider,
+    DetailImage,
+    CommentText,
+    CommentCount,
   },
 );
 
