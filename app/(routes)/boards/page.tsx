0a4f20dcdd15ec99a32003c959c7a0b53a19e77d
jeongmin <jmini9218@gmail.com>
2025-02-18 23:43:58 +0900

    feat: 게시판 메인 리액트쿼리로 변경, 게시글 이동/삭제 등 추가

--- app/(routes)/boards/page.tsx
+++ app/(routes)/boards/page.tsx
@@ -6,94 +6,89 @@ import BestArticleList from '@/_components/boards/BestArticleList';
 import ArticleCard from '@/_components/boards/ArticleCard';
 import SortDropdown from '@/_components/boards/SortDropdown';
 import InfiniteScroll from 'react-infinite-scroll-component';
-import { Article } from './type/Article';
-import { getArticles } from '@/_lib/api/article-api';
+import { Article } from './type/Boards';
+import { useArticle } from '@/_hooks/useArticle';
+import { Divider } from '@/_components/boards/Card';
+import { useRouter } from 'next/navigation';
+import { useAuthStore } from '@/_store/auth-store';
 
+const pageSize = 6;
 const BoardsPage = () => {
-  const handleButton = () => alert('글쓰기');
+  const { isLoggedIn } = useAuthStore();
+  const router = useRouter();
   const [articles, setArticles] = useState<Article[]>([]);
   const [sortOrder, setSortOrder] = useState<'recent' | 'like'>('recent');
   const [page, setPage] = useState<number>(1);
   const [keyword, setKeyword] = useState<string>('');
   const [hasMore, setHasMore] = useState<boolean>(true);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const pageSize = 4;
 
-  const fetchArticles = async (
-    page = 1,
-    order: 'recent' | 'like' = 'recent',
-    searchKeyword = '',
-  ) => {
-    try {
-      setIsLoading(true);
-      const response = await getArticles({
-        page: page,
-        pageSize: pageSize,
-        orderBy: order,
-        keyword: searchKeyword,
-      });
-      console.log('리스폰스:', response);
-      const { list } = response;
+  const { useGetArticles } = useArticle();
+  const { data, isLoading, isError } = useGetArticles({
+    page,
+    pageSize,
+    orderBy: sortOrder,
+    keyword,
+  });
+
+  useEffect(() => {
+    if (data) {
+      const { list } = data;
 
       if (page === 1) {
         setArticles(list);
       } else {
-        setArticles((prevArticles) => [
-          ...prevArticles,
-          ...list.filter(
-            (article: Article) =>
-              !prevArticles.some(
-                (prevArticle) => prevArticle.id === article.id,
-              ),
-          ),
-        ]);
+        setArticles((prevArticles) => {
+          const newArticles = list.filter(
+            (newArticle: Article) =>
+              !prevArticles.some((article) => article.id === newArticle.id),
+          );
+          return [...prevArticles, ...newArticles];
+        });
       }
-
-      if (list.length < pageSize) {
-        setHasMore(false);
-      } else {
-        setHasMore(true);
-      }
-    } catch (error) {
-      console.error('Error:', error);
-      setHasMore(false);
-    } finally {
-      setIsLoading(false);
+      setHasMore(list.length === pageSize);
     }
-  };
-
-  useEffect(() => {
-    setArticles([]);
-    setPage(1);
-    setHasMore(true);
-    fetchArticles(1, sortOrder, keyword);
-  }, [keyword, sortOrder]);
-
-  useEffect(() => {
-    if (page > 1) {
-      fetchArticles(page, sortOrder, keyword);
-    }
-  }, [page, sortOrder, keyword]);
+  }, [data, page]);
 
   // 정렬 순서
-  const handleSelect = (value: string) => {
-    if (value === 'recent' || value === 'like') {
-      setSortOrder(value);
-      setPage(1);
-    } else {
-      console.error('Invalid value:', value);
-    }
+  const handleSelect = (value: 'recent' | 'like') => {
+    setSortOrder(value);
+    setPage(1);
   };
+
   // 검색
   const handleSearchChange = (searchKeyword: string) => {
     setKeyword(searchKeyword);
     setPage(1);
+    setSortOrder('recent');
   };
-   // 검색 결과 없음
-   const searchNoResults = keyword && articles.length === 0 && !isLoading;
+
+  // 검색 결과 없음
+  const searchNoResults = keyword && articles.length === 0 && !isLoading;
+
+  // 게시글 삭제
+  const handleDelete = (articleId: number) => {
+    setArticles((prevArticles) =>
+      prevArticles.filter((article) => article.id !== articleId),
+    );
+  };
+
+  // 게시글 이동
+  const handleCardClick = (articleId: number) => {
+    router.push(`/boards/${articleId}`);
+  };
+
+  const handleButton = () => {
+    router.push('/boards/create');
+  }
+
+  // 오류
+  if (isError) {
+    alert('다시 시도해 주세요.');
+    router.push('/');
+  }
 
   return (
-    <div className="relative pt-[3.2rem] tablet:pt-[4rem]">
+    <div className="relative cursor-default py-[3.2rem] tablet:py-[4rem]">
       <h2 className="text-lg font-bold tablet:text-2xl">자유게시판</h2>
       <div className="my-[2.4rem] tablet:my-[3.2rem] pc:my-[4rem]">
         <Search onChange={handleSearchChange} />
@@ -104,11 +99,11 @@ const BoardsPage = () => {
           <h3 className="mb-[2.4rem] font-medium tablet:mb-[4rem] tablet:text-xl tablet:font-semibold">
             베스트 게시글
           </h3>
-          <BestArticleList />
+          <BestArticleList onArticleClick={handleCardClick} />
         </div>
       )}
 
-      <div className="my-[3.2rem] h-[1px] w-full bg-text-primary opacity-[10%] tablet:my-[4rem]"></div>
+      <Divider className="my-[3.2rem] tablet:my-[4rem]" />
 
       <div className="mb-[2.4rem] flex items-center justify-between tablet:mb-[4rem]">
         <h3 className="font-medium tablet:text-xl tablet:font-semibold">
@@ -120,7 +115,7 @@ const BoardsPage = () => {
       </div>
 
       {searchNoResults && (
-        <div className="p-[1.6rem] tablet:py-[2rem] text-center tablet:text-lg text-text-disabled">
+        <div className="p-[1.6rem] text-center text-text-disabled tablet:py-[2rem] tablet:text-lg">
           검색 결과가 없습니다.
         </div>
       )}
@@ -132,22 +127,29 @@ const BoardsPage = () => {
         loader={<div>Loading...</div>}
       >
         <div className="grid grid-cols-1 gap-[1.6rem] tablet:gap-[2.4rem] tablet:gap-x-[2rem] pc:grid-cols-2">
-          {articles.map((article) => (
-            <ArticleCard key={article.id} article={article} />
+          {articles.map((article: Article) => (
+            <ArticleCard
+              key={article.id}
+              article={article}
+              onArticleClick={() => handleCardClick(article.id)}
+              onDelete={() => handleDelete(article.id)}
+            />
           ))}
         </div>
       </InfiniteScroll>
 
-      <div className="fixed bottom-[6.2rem] right-[1.6rem] tablet:bottom-[4.5rem] tablet:right-[2.4rem] pc:right-[6.5rem]">
-        <Button
-          icon="plus"
-          round="full"
-          onClick={handleButton}
-          className="w-[10.4rem] text-[1.5rem]"
-        >
-          글쓰기
-        </Button>
-      </div>
+      {isLoggedIn && (
+        <div className="fixed bottom-[6.2rem] right-[1.6rem] tablet:bottom-[4.5rem] tablet:right-[2.4rem] pc:right-[6.5rem]">
+          <Button
+            icon="plus"
+            round="full"
+            onClick={handleButton}
+            className="w-[10.4rem] text-[1.5rem]"
+          >
+            글쓰기
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
